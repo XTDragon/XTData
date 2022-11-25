3cd5d94b524207a73ded07c3406e51fbc30489a3
XTDragon <1686365414@qq.com>
2022-11-25 11:01:30 +0800

    XTData-jenkinsTest

--- src/main/java/com/xtdragon/xtdata/controller/TestController.java
+++ src/main/java/com/xtdragon/xtdata/controller/TestController.java
@@ -1,12 +1,16 @@
 package com.xtdragon.xtdata.controller;
 
+import com.xtdragon.xtdata.service.PoetryService;
 import com.xtdragon.xtdata.utils.CommonResult;
 import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TestController {
+    @Autowired
+    PoetryService poetryService;
 
     @RequestMapping("/")
     public String helloWorld() {
@@ -25,5 +29,13 @@ public class TestController {
     public String userGet() {
         System.out.println("HelloWorld");
         return "/index";
+    }
+
+
+    @RequestMapping("/test")
+    public String test(){
+        System.out.println("test");
+        System.out.println(poetryService.getPoetry());
+        return "/main";
     }
 }
