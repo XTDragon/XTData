a1c7b68c1cd2016422ed81c4103bfbf240c7a493
XTDragon <1686365414@qq.com>
2022-12-06 16:38:29 +0800

    XTData-jenkinsTest

---
+++ src/main/java/com/xtdragon/xtdata/service/Imp/PoetryServiceImpl.java
@@ -0,0 +1,23 @@
+package com.xtdragon.xtdata.service.Imp;
+
+import com.xtdragon.xtdata.service.PoetryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class PoetryServiceImpl implements PoetryService {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Override
+    public String getPoetry() {
+        String url="https://github.com/chinese-poetry/chinese-poetry/blob/master/json/poet.song.0.json";
+        //依赖于GitHub项目https://github.com/NateScarlet/holiday-cn
+        HttpEntity entity = restTemplate.exchange(url, HttpMethod.GET,null, String.class);
+        return entity.getBody().toString();
+    }
+}
